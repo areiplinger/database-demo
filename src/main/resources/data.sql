ac1e113fbed6bfbeb1079133d8617dd08449ec6c
Andrew Reiplinger <areiplinger@medimpact.com>
2018-07-10 15:52:37 -0700

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+CREATE TABLE person
+(
+  id integer not null,
+  name varchar(255) not null,
+  location varchar(255),
+  birth_date timestamp,
+  PRIMARY KEY(id)
+  );
+
+
+INSERT INTO PERSON(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES (10020, 'JOHN SMITH', 'ANYTOWN', sysdate());
+INSERT INTO PERSON(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES (10021, 'mike jones', 'MYTOWN', sysdate());
+INSERT INTO PERSON(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES (10022, 'JANE DOE', 'YOURTOWN', sysdate());
